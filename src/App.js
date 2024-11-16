d58fdf48bcf2f77a0ef822171c72d77bb78f88f3
igborre <iborreani@correo.um.edu.uy>
2024-11-16 05:17:29 -0300

    YA NO SE1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles/App.css";
-import {Route, Routes, useNavigate} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LogIn from "./Pages/LogInPage.js";
 import SignUp from "./Pages/RegisterPage.js";
 import Reserva from "./Components/Reserva.js";
@@ -12,44 +12,41 @@ import Home from "./Pages/HomePage.js";
 import { Link } from "react-router-dom";
 import logo from "./logo.jpeg";
 import Button from "./Components/Button.js";
-import Cookies from "js-cookie";
 import Profile from "./Pages/ProfilePage.js";
 import Logout from "./Pages/LogoutPage.js";
-import Auth from "./Utils/Auth"
+import useAuth from "./Utils/Auth";
 
-
-// Main App Component
 const App = () => {
-    let { isLoggedIn } = Auth();
-    console.log(isLoggedIn);
+    const { isLoggedIn } = useAuth();
 
-  return (
+    return (
         <div className="App">
-          <header className="App-header">
-            <Link to="/">
-              <img src={logo} className="App-logo" alt="logo" />
-            </Link>
-            <Link to="/movieUpload">
-              <Button>Upload a Movie</Button>
-            </Link>
-            <Button onClick={isLoggedIn = false}> restart</Button>
-            {!isLoggedIn ? <ProfileMenu /> : <LoggedOutHeader />}
-          </header>
+            <header className="App-header">
+                <Link to="/">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </Link>
+                {isLoggedIn && (
+                    <Link to="/movieUpload">
+                        <Button>Upload a Movie</Button>
+                    </Link>
+                )}
+                {isLoggedIn ? <ProfileMenu /> : <LoggedOutHeader />}
+            </header>
 
-          <div className="App-body">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/logIn" element={<LogIn />} />
-              <Route path="/signUp" element={<SignUp />} />
-              <Route path="/reserva" element={<Reserva />} />
-              <Route path="/movies" element={<MovieList />} />
-              <Route path="/movieUpload" element={<MovieUpload />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/logout" element={<Logout />} />
-            </Routes>
-          </div>
+            <div className="App-body">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/logIn" element={<LogIn />} />
+                    <Route path="/signUp" element={<SignUp />} />
+                    <Route path="/reserva" element={<Reserva />} />
+                    <Route path="/movies" element={<MovieList />} />
+                    <Route path="/movieUpload" element={<MovieUpload />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/logout" element={<Logout />} />
+                </Routes>
+            </div>
         </div>
-  );
+    );
 };
 
 export default App;
